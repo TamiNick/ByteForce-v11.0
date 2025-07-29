8b7c5a6b14b21fa2098b2e1163c8ae1030e965a7
Andrii <kans.one12@gmail.com>
2025-07-29 18:32:35 +0300

    update

--- src/main.js
+++ src/main.js
@@ -2,29 +2,18 @@
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      modal: document.querySelector("[data-modal-]"),
     };
+
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+
     function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-open");
       document.body.classList.toggle("no-scroll");
     }
 })();
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-second-open]"),
-      closeModalBtn: document.querySelector("[data-modal-second-close]"),
-      modal: document.querySelector("[data-modal-second]"),
-    };
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-})();
 
 (() => {
     const refs = {
@@ -35,8 +24,22 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
     function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-open");
       document.body.classList.toggle("no-scroll");
     }
-  })();
+})();
 
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-second-open]"),
+      closeModalBtn: document.querySelector("[data-modal-second-close]"),
+      modal: document.querySelector("[data-modal-second]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+      document.body.classList.toggle("no-scroll");
+    }
+})();
