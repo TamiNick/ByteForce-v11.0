82e928b97dfe7208775008fd9a249bd1938015f6
TamiNick <dmytriy.litvin@gmail.com>
2025-07-29 17:48:39 +0300

    fix

--- src/js/modal-third.js
+++ src/js/modal-third.js
@@ -1,17 +1,4 @@
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-third-open]"),
-      closeModalBtn: document.querySelector("[data-modal-third-close]"),
-      modal: document.querySelector("[data-modal-third]"),
-    };
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
- {
-input ('./js/mobie-menu.js')
-input ('./js/modal.js')
-input ('./js/modal-second.js')
-input ('./js/modal-third.js')
- }
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal-]"),
+    };
 
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+})();
+
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-third-open]"),
+      closeModalBtn: document.querySelector("[data-modal-third-close]"),
+      modal: document.querySelector("[data-modal-third]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
+
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-second-open]"),
+      closeModalBtn: document.querySelector("[data-modal-second-close]"),
+      modal: document.querySelector("[data-modal-second]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+})();
 
   
 
