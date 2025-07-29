64c4f87a43c2fdb1759df54475f97c40c9962e5b
Darinka <darina.myshka@gmail.com>
2025-07-29 17:28:18 +0300

    update

--- src/main.js
+++ src/main.js
@@ -12,4 +12,42 @@
       refs.modal.classList.toggle("is-open");
       document.body.classList.toggle("no-scroll");
     }
+})();
+  
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-second-open]"),
+      closeModalBtn: document.querySelector("[data-modal-second-close]"),
+      modal: document.querySelector("[data-modal-second]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+})();
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-third-open]"),
+      closeModalBtn: document.querySelector("[data-modal-third-close]"),
+      modal: document.querySelector("[data-modal-third]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
   })();
+
+
+
+
+
+
+
+
+
+
+
